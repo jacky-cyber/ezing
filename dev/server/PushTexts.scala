fc7e58b64dcc83991149ce95326920f8f396d57f
ljshj <jinshanl@gmail.com>
2016-01-03 17:29:17 +0800

    移除－》移出

--- dev/server/PushTexts.scala
+++ dev/server/PushTexts.scala
@@ -3,7 +3,7 @@ package im.actor.server.office
 object PushTexts {
   val Added = "有人加入群组"
   val Invited = "你被邀请加入群组"
-  val Kicked = "有人被移除群组"
+  val Kicked = "有人被移出群组"
   val Left = "有人离开群组"
   val TitleChanged = "群组名称已修改"
   val TopicChanged = "群组主题已修改"
